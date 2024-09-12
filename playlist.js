11548224683358b0474b8d3453c0d21c3371c6b4
Sivadharshini-Patchaimuthu <sivadharshinipatchaimuthu@gmail.com>
2024-09-12 18:44:43 +0530

    Add files via upload

---
+++ playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+        name : "Believer",
+        artist:"Imagine Dragons",
+        img:"believer",
+        audio:"Believer"
+    },
+    {
+        name : "Night Changes",
+        artist:"One Direction",
+        img:"nightchanges",
+        audio:"Night Changes"
+    },
+    {
+        name : "Celebration Of Life",
+        artist:"Navin Iyer, G. V. Prakash Kumar",
+        img:"CelebrationOfLife",
+        audio:"Celebrationoflife"
+    },
+    {
+        name : "Perfect",
+        artist:"Ed Sheeran",
+        img:"perfect",
+        audio:"Perfect"
+    }
+]
